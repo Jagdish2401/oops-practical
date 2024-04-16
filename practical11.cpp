a7111a71ea468ef978fbb6bbbeeadafcf14bb2b5
Jagdish2401 <149369569+Jagdish2401@users.noreply.github.com>
2024-04-16 09:42:57 +0000

    Create practical11.cpp

---
+++ practical11.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <iomanip>
+using namespace std;
+class employ
+{
+    int year,month;
+public:
+    int getdata()
+    {
+        cout<<"Enter employ experiance in years and month"<<endl;
+        cout<<"years:";
+        cin>>year;
+        cout<<"month:";
+        cin>>month;
+        return 0;
+    }
+    operator float()
+    {
+        float temp;
+        temp = year + (month)/12.0;
+        return temp;
+    }
+
+};
+int main()
+{
+    employ o1;
+    o1.getdata();
+    float n;
+    n=o1;
+    cout<<"Employ experiance is:"<<n;
+}
