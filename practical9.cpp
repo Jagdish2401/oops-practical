61de863bd0269232b5e9f4916d1fc4fdce6f4431
Jagdish2401 <149369569+Jagdish2401@users.noreply.github.com>
2024-05-13 16:13:04 +0530

    Update practical9.cpp

--- practical9.cpp
+++ practical9.cpp
@@ -1,1 +1,95 @@
+#include<iostream>
+using namespace std;
+
+class mm;
+//mm to cm
+class cm
+{
+    float a;
+public:
+    float getdata()
+    {
+        cout<<"Enter the milimeter :";
+        cin>>a;
+        return 0;
+    }
+    float printdata()
+    {
+        return a;
+    }
+    cm()
+    {
+        a=10;
+    }
+    cm(mm &n);
+};
+
+class mm
+{
+    float b;
+public:
+    float getdata()
+    {
+        cout<<"Enter the cm :";
+        cin>>b;
+        return 0;
+    }
+    float printdata()
+    {
+        return b;
+    }
+    mm()
+    {
+        b=10;
+    }
+    mm(cm &n)
+    {
+        b=n.printdata()/10;
+    }
+};
+cm :: cm(mm &n)
+{
+    a=n.printdata()*10;
+
+}
+int main()
+{
+    int ch;
+
+    cout<<"Enter 1 convert mm to cm "<<endl<<"Enter 2 convert cm to mm"<<endl;
+    cin>>ch;
+    switch(ch)
+    {
+    case 1:
+       {
+           cm o1;mm o2;
+        o1.getdata();
+        o2=o1;
+       cout<< o2.printdata();
+        break;
+        }
+    case 2:
+       {
+        mm o1;cm o2;
+        o1.getdata();
+        o2=o1;
+       cout<< o2.printdata();
+        break;
+    }
+    default:
+        exit;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 
